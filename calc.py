0ada6c1e194d2c8052c9fa0ea78fc0fde63736fa
kcw001 <130221363+kcw001@users.noreply.github.com>
2023-04-12 08:57:09 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -3,53 +3,23 @@
 def plus(x, y):
     return x+y
 
-def subtraction(x,y):
-    return x-y
-
-def multiplication(x,y):
-    return x*y
-
-def division(x,y):
-    if y == 0:
-	print("0으로 나눌 수 없습니다.")
-    else:
-	return x/y
-
 # main function
 def main():
+    check = 1
     print("Welcome to calcuator")
-    while True
-        print("0: exit, 1: plus, 2: subtraction, 3: multiplication, 4: division")
-        check = input()
-        if check == "1":
+    while check >= 1:        
+        print("0: exit, 1: plus")
+        check = int(input())
+        if check == 1:
             print("First Number")
-            x = float(input())
+            x = int(input())
             print("Second Number")
-            y = float(input())
+            y = int(input())
             print("answer : ", plus(x,y))
-        elif check == "2":
-            print("First Number")
-            x = float(input())
-            print("Second Number")
-            y = float(input())
-            print("answer : ", subtration(x,y))
-	elif  check == "3":
-            print("First Number")
-            x = float(input())
-            print("Second Number")
-            y = float(input())
-            print("answer : ", multiplication(x,y))
-	elif  check == "4":
-            print("First Number")
-            x = float(input())
-            print("Second Number")
-            y = float(input())
-            print("answer : ", division(x,y))
-	elif check == "0":
-	    print("Thank you")
-	    break
+        elif check > 1:
+            print("Unsupported")
         else:
-            print("Please enter number")
+            print("Thank you")
 
 if __name__ == "__main__":
     main()
